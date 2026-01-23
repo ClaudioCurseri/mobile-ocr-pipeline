1fcede4f7221616f4ecacaf3e6f279588b63c497
Claudio Curseri <claudio.curseri@outlook.de>
2026-01-23 18:43:31 +0100

    chore: clean up project structure

--- text_recognition_pipeline/pipeline/postprocessing.py
+++
@@ -1,4 +0,0 @@
-# TODO: Implement postprocessing
-
-class Postprocessing:
-    pass
--- text_recognition_pipeline2/TextRecognition.cpp
+++
@@ -1,8 +0,0 @@
-#include "TextRecognition.h"
-#include <iostream>
-#include <opencv2/opencv.hpp>
-
-std::string TextRecognition::recognize(const cv::Mat& image) const {
-    return api->GetUTF8Text();
-}
-
