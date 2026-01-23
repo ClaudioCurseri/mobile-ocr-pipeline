250bad0844bfd7527f28a34469b34c1942ab5710
Claudio Curseri <claudio.curseri@outlook.de>
2026-01-23 18:31:52 +0100

    feat: finish preprocessing

--- text_recognition_pipeline2/Preprocessing.cpp
+++ text_recognition_pipeline2/Preprocessing.cpp
@@ -1,26 +1,10 @@
 #include "Preprocessing.h"
 #include <opencv2/opencv.hpp>
 
-Preprocessing::Preprocessing() {
-    this->api = new tesseract::TessBaseAPI();
-    if (this->api->Init(nullptr, "osd", tesseract::OEM_TESSERACT_ONLY)) {
-        std::cerr << "Could not initialize tesseract." << std::endl;
-        return;
-    }
-    this->api->SetPageSegMode(tesseract::PageSegMode::PSM_OSD_ONLY);
-}
-
-Preprocessing::~Preprocessing() {
-    this->api->End();
-    delete this->api;
-}
-
 cv::Mat Preprocessing::preprocessingStep() {
-    // TODO: implement all preprocessing steps
-    convertToGrayscale();
-    convertToBinary();
-    medianFilter();
-    correctPageOrientation();
+    convertToBinaryImage();
+    applyMedianFilter();
+    dewarpImage();
     resizeImage();
     this->image = this->internalImage.clone();
     return this->image;
@@ -37,40 +21,12 @@ void Preprocessing::showImage() const {
     cv::waitKey(0);
 }
 
-void Preprocessing::correctPageOrientation() {
-    // set tesseract image
-    this->api->SetImage(this->internalImage.data, this->internalImage.cols, this->internalImage.rows,
-                        this->internalImage.channels(), this->internalImage.step);
-    // perform osd
-    int orient_deg;
-    float orient_conf;
-    const char* script_name;
-    float script_conf;
-    this->api->DetectOrientationScript(&orient_deg, &orient_conf, &script_name, &script_conf);
-    // correct orientation
-    switch (orient_deg) {
-        case 90:
-            cv::rotate(this->internalImage, this->internalImage, cv::ROTATE_90_COUNTERCLOCKWISE);
-            break;
-        case 180:
-            cv::rotate(this->internalImage, this->internalImage, cv::ROTATE_180);
-            break;
-        case 270:
-            cv::rotate(this->internalImage, this->internalImage, cv::ROTATE_90_CLOCKWISE);
-            break;
-        default:
-    }
-}
-
-void Preprocessing::convertToGrayscale() {
+void Preprocessing::convertToBinaryImage() {
     cv::cvtColor(this->internalImage, this->internalImage, cv::COLOR_RGB2GRAY);
-}
-
-void Preprocessing::convertToBinary() {
     cv::adaptiveThreshold(this->internalImage, this->internalImage, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 9, 5);
 }
 
-void Preprocessing::medianFilter() {
+void Preprocessing::applyMedianFilter() {
     cv::medianBlur(this->internalImage, this->internalImage, 5);
 }
 
@@ -81,3 +37,95 @@ void Preprocessing::resizeImage() {
     cv::resize(this->internalImage, this->internalImage, targetSize);
 }
 
+std::vector<cv::Point2f> orderPoints(const std::vector<cv::Point>& pts) {
+    std::vector<cv::Point2f> rect(4);
+
+    auto sumComp = [](const cv::Point& a, const cv::Point& b) { return a.x + a.y < b.x + b.y; };
+    auto diffComp = [](const cv::Point& a, const cv::Point& b) { return a.y - a.x < b.y - b.x; };
+
+    // find top left (min sum) and bottom right (max sum)
+    const auto [tl, br] = std::minmax_element(pts.begin(), pts.end(), sumComp);
+    rect[0] = *tl;
+    rect[2] = *br;
+
+    // find top right (min diff) and bottom left (max diff)
+    const auto [tr, bl] = std::minmax_element(pts.begin(), pts.end(), diffComp);
+    rect[1] = *tr;
+    rect[3] = *bl;
+
+    return rect;
+}
+
+void Preprocessing::dewarpImage() {
+    // edge detection
+    cv::Mat edged;
+    cv::Canny(this->internalImage, edged, 30, 100);
+
+    // dilation -> all structures grow, increases chance of finding the document contours
+    cv::Mat dilated;
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(11, 11));
+    cv::dilate(edged, dilated, kernel, cv::Point(-1, -1), 2);
+
+    // find all contours
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(dilated, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    // sort found contours in descending order
+    std::ranges::sort(contours, [](const std::vector<cv::Point>& c1, const std::vector<cv::Point>& c2) {
+        return cv::contourArea(c1) > cv::contourArea(c2);
+    });
+
+    std::vector<cv::Point> documentContour;
+    bool found = false;
+
+    // calculate total image area
+    double imageArea = this->internalImage.rows * this->internalImage.cols;
+
+    // find the document contours
+    for (const auto& contour : contours) {
+        // do not accept contours that are too small
+        if (double area = cv::contourArea(contour); area < imageArea * 0.1) {
+            continue;
+        }
+
+        double perimeter = cv::arcLength(contour, true);
+        std::vector<cv::Point> approx;
+
+        cv::approxPolyDP(contour, approx, 0.03 * perimeter, true);
+
+        if (approx.size() == 4) {
+            documentContour = approx;
+            found = true;
+            break;
+        }
+    }
+
+    // document contours not found
+    if (!found) {
+        std::cerr << "Unable to find document contours. Returning original image." << std::endl;
+        return;
+    }
+
+    // transform the image
+    std::vector<cv::Point2f> rect = orderPoints(documentContour);
+
+    // calculate new image dimensions
+    float widthA = static_cast<float>(cv::norm(rect[2] - rect[3])); // BR - BL
+    float widthB = static_cast<float>(cv::norm(rect[1] - rect[0])); // TR - TL
+    int maxWidth = std::max(static_cast<int>(widthA), static_cast<int>(widthB));
+
+    float heightA = static_cast<float>(cv::norm(rect[1] - rect[2])); // TR - BR
+    float heightB = static_cast<float>(cv::norm(rect[0] - rect[3])); // TL - BL
+    int maxHeight = std::max(static_cast<int>(heightA), static_cast<int>(heightB));
+
+    std::vector destinationPoints = {
+        cv::Point2f(0, 0),
+        cv::Point2f(static_cast<float>(maxWidth) - 1, 0),
+        cv::Point2f(static_cast<float>(maxWidth) - 1, static_cast<float>(maxHeight) - 1),
+        cv::Point2f(0, static_cast<float>(maxHeight) - 1)
+    };
+
+    // apply image transformation
+    cv::Mat M = cv::getPerspectiveTransform(rect, destinationPoints);
+    cv::warpPerspective(this->internalImage, this->internalImage, M, cv::Size(maxWidth, maxHeight));
+}
--- text_recognition_pipeline2/Preprocessing.h
+++ text_recognition_pipeline2/Preprocessing.h
@@ -1,25 +1,20 @@
 #ifndef PREPROCESSING_H
 #define PREPROCESSING_H
-#include <tesseract/baseapi.h>
 #include <opencv2/opencv.hpp>
 
 
 class Preprocessing {
 public:
-    Preprocessing();
-    ~Preprocessing();
     void setImage(const cv::Mat &image);
     cv::Mat preprocessingStep();
     void showImage() const;
 private:
-    tesseract::TessBaseAPI *api;
     cv::Mat image;
     cv::Mat internalImage;
-    void correctPageOrientation();
-    void convertToGrayscale();
-    void convertToBinary();
-    void medianFilter();
+    void convertToBinaryImage();
+    void applyMedianFilter();
     void resizeImage();
+    void dewarpImage();
 };
 
 
