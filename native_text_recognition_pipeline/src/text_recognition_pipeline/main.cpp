9a7a94806d66d3bf65bcce18671d0e67a93e0152
Claudio Curseri <claudio.curseri@outlook.de>
2026-01-31 09:05:56 +0100

    chore: make main.ccp more readable

--- native_text_recognition_pipeline/src/text_recognition_pipeline/main.cpp
+++ native_text_recognition_pipeline/src/text_recognition_pipeline/main.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 #include "TextRecognitionPipeline.h"
 
+/**
+ * The main entry point of the text recognition pipeline.
+ * This is used to perform a performance evaluation by running the pipeline on a test dataset.
+ * Each result is saved in a new text file.
+ * @return Whether the execution was successful.
+ */
 int main() {
-    long imageNum;
     // initialize text recognition pipeline
     auto *textRecognition = new TextRecognitionPipeline();
     // initialize dictionaries
@@ -17,7 +22,6 @@ int main() {
     if (textRecognition->initBigramDictionary("./dictionary/frequency_bigramdictionary_en_243_342.txt")) {
         std::cerr << "Could not initialize bigram dictionary." << std::endl;
     }
-    // ----------run the text recognition pipeline----------
     const std::filesystem::path inputDir = "./../../../evaluation/testDataset/input_test/";
     const std::filesystem::path outputDir = "./../../../evaluation/testDataset/output_test/";
     // ensure input directory exists
@@ -29,66 +33,64 @@ int main() {
     if (!std::filesystem::exists(outputDir)) {
         std::filesystem::create_directories(outputDir);
     }
-    // check whether it should only test one image or run for the whole dataset
     std::cout << "Running the pipeline..." << std::endl;
-    imageNum = 1;
-        for (const auto& entry : std::filesystem::directory_iterator(inputDir)) {
-            if (entry.path().extension() == ".jpg") {
-                // construct input filepath
-                std::ostringstream filenameStream;
-                filenameStream << std::setw(5) << std::setfill('0') << imageNum << ".jpg";
-                std::string filename = filenameStream.str();
-                std::filesystem::path inputPath = inputDir / filename;
+    long imageNum = 1;
+    for (const auto& entry : std::filesystem::directory_iterator(inputDir)) {
+        if (entry.path().extension() == ".jpg") {
+            // construct input filepath
+            std::ostringstream filenameStream;
+            filenameStream << std::setw(5) << std::setfill('0') << imageNum << ".jpg";
+            std::string filename = filenameStream.str();
+            std::filesystem::path inputPath = inputDir / filename;
 
-                // construct output filepath
-                std::filesystem::path outputFilename = std::filesystem::path(filename).replace_extension(".txt");
-                std::filesystem::path outputPath = outputDir / outputFilename;
+            // construct output filepath
+            std::filesystem::path outputFilename = std::filesystem::path(filename).replace_extension(".txt");
+            std::filesystem::path outputPath = outputDir / outputFilename;
 
-                // ensure input file exists
-                if (!std::filesystem::exists(inputPath)) {
-                    std::cerr << "File " << filename << " does not exist. Skipping." << std::endl;
-                    continue;
-                }
-                // pipeline configuration
-                auto preprocessingConfig = TextRecognitionPipeline::PreprocessingConfig{
-                    true,
-                    true,
-                    true,
-                    true,
-                    true
-                };
-                auto postprocessingConfig = TextRecognitionPipeline::PostprocessingConfig{
-                    true,
-                    false
-                };
-                // run the pipeline
-                textRecognition->initTesseract();
-                textRecognition->setImage(inputPath.string().c_str());
-                textRecognition->preprocessingStep(preprocessingConfig);
-                textRecognition->textRecognitionStep();
-                textRecognition->postprocessingStep(postprocessingConfig);
-                auto finalResult = textRecognition->getRecognitionResult();
-                // save results
-                if (std::ofstream outFile(outputPath); outFile.is_open()) {
-                    for (const auto& s : finalResult) {
-                        outFile << std::get<4>(s);
-
-                        if (std::get<5>(s)) {
-                            outFile << "\n";
-                        } else {
-                            outFile << " ";
-                        }
-                    }
-                    outFile.close();
-                    std::cout << "  > Saved result to: " << outputFilename << std::endl;
-                } else {
-                    std::cerr << "Error: Could not write to " << outputPath << std::endl;
-                }
-                imageNum += 1;
+            // ensure input file exists
+            if (!std::filesystem::exists(inputPath)) {
+                std::cerr << "File " << filename << " does not exist. Skipping." << std::endl;
+                continue;
             }
+            // pipeline configuration
+            auto preprocessingConfig = TextRecognitionPipeline::PreprocessingConfig{
+                true,
+                true,
+                true,
+                true,
+                true
+            };
+            auto postprocessingConfig = TextRecognitionPipeline::PostprocessingConfig{
+                true,
+                false
+            };
+            // run the pipeline
+            textRecognition->initTesseract();
+            textRecognition->setImage(inputPath.string().c_str());
+            textRecognition->preprocessingStep(preprocessingConfig);
+            textRecognition->textRecognitionStep();
+            textRecognition->postprocessingStep(postprocessingConfig);
+            auto finalResult = textRecognition->getRecognitionResult();
+            // save results
+            if (std::ofstream outFile(outputPath); outFile.is_open()) {
+                for (const auto& s : finalResult) {
+                    // the current word
+                    outFile << std::get<4>(s);
+                    // ensure correct output format
+                    if (std::get<5>(s)) {
+                        outFile << "\n";
+                    } else {
+                        outFile << " ";
+                    }
+                }
+                outFile.close();
+                std::cout << "  > Saved result to: " << outputFilename << std::endl;
+            } else {
+                std::cerr << "Error: Could not write to " << outputPath << std::endl;
+            }
+            imageNum += 1;
         }
-
-    // ----------end of the text recognition pipeline----------
+    }
     // free memory
     delete textRecognition;
     std::cout << "Done." << std::endl;
